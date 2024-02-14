e62aead937a90a4bf9eec09b4de22e0715a43fe5
Aaryan Bajaj <aaryanbajaj385@gmail.com>
2024-02-14 07:43:38 +0530

    active links

---
+++ routing-demo/src/app/(auth)/layout.tsx
@@ -0,0 +1,28 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import './styles.css'
+
+const navLinks = [
+    { name: "Register", href: "/register" },
+    { name: "Login", href: "/login" },
+    { name: "Forgot Password", href: "/forgot-password" },
+];
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const pathname = usePathname();
+  return (
+      <div>
+          {navLinks.map((link) => (
+            <Link href={link.href} key={link.name} className={pathname.startsWith(link.href) ? "font-bold mr-3" : "text-blue-500 mr-4"}>
+                {link.name}
+            </Link>
+          ))}
+          {children}
+      </div>
+  );
+}
