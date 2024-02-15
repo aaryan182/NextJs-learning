35ab30cf15132d28b38d99bec1b2a17ba95ed3d0
Aaryan Bajaj <aaryanbajaj385@gmail.com>
2024-02-15 11:17:43 +0530

    cookies in route handlers

--- route-handlers-demo/src/app/profile/api/route.ts
+++ route-handlers-demo/src/app/profile/api/route.ts
@@ -1,15 +1,22 @@
 import { type NextRequest } from "next/server";
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 
 export async function GET(request: NextRequest) {
   const requestHeaders = new Headers(request.headers);
   const headerList = headers();
+
+  cookies().set("resultsPerPage", "10");
+  const theme = request.cookies.get("theme");
   console.log(requestHeaders.get("authorization"));
   console.log(headerList.get("authorization"));
+  console.log(theme);
+  console.log(cookies().get("resultsPerPage"));
+  
 
   return new Response("<h1>profile api data ! </h1>", {
     headers: {
       "Content-Type": "text/html",
+      "Set-Cookie": "theme=dark",
     },
   });
 }
