d1cb8cb8bd924843bfda424086108a6c640bd2a0
Aaryan Bajaj <aaryanbajaj385@gmail.com>
2024-02-15 11:12:29 +0530

    headers in route handlers

--- route-handlers-demo/src/app/profile/api/route.ts
+++ route-handlers-demo/src/app/profile/api/route.ts
@@ -1,3 +1,15 @@
-export async function GET() {
-    return new Response("profile api data !");
+import { type NextRequest } from "next/server";
+import { headers } from "next/headers";
+
+export async function GET(request: NextRequest) {
+  const requestHeaders = new Headers(request.headers);
+  const headerList = headers();
+  console.log(requestHeaders.get("authorization"));
+  console.log(headerList.get("authorization"));
+
+  return new Response("<h1>profile api data ! </h1>", {
+    headers: {
+      "Content-Type": "text/html",
+    },
+  });
 }
